c63839859b5c75be623d56ae73e416a5add496db
Zacarias <zaca03zaca@gmail.com>
2024-07-15 23:43:13 -0300

    Fix duplicate fields in CustomUserAdmin fieldsets

---
+++ resources/users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+	add_fieldsets = (
+		(
+			None,
+			{
+				"classes": ("wide",),
+				"fields": (
+					"username",
+					"password1",
+					"password2",
+					"email",
+					"is_staff",
+					"is_customer",
+				),
+			},
+		),
+	)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
