ad4151a6fdae5751b955d7689f13a8a2bae4b5b5
Zacarias <zaca03zaca@gmail.com>
2024-07-16 23:25:51 -0300

    Add field for sellers

--- resources/users/admin.py
+++ resources/users/admin.py
@@ -5,22 +5,23 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-	add_fieldsets = (
-		(
-			None,
-			{
-				"classes": ("wide",),
-				"fields": (
-					"username",
-					"password1",
-					"password2",
-					"email",
-					"is_staff",
-					"is_customer",
-				),
-			},
-		),
-	)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "email",
+                    "is_staff",
+                    "is_customer",
+                    "is_salesperson",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- resources/users/models.py
+++ resources/users/models.py
@@ -3,19 +3,20 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-	is_staff = models.BooleanField(default=False)
-	is_customer = models.BooleanField(default=True)
-	groups = models.ManyToManyField(
-		Group,
-		related_name="customuser_set",
-		blank=True,
-		help_text="The groups this user belongs to.",
-		verbose_name="groups",
-	)
-	user_permissions = models.ManyToManyField(
-		Permission,
-		related_name="customuser_set",
-		blank=True,
-		help_text="Specific permissions for this user.",
-		verbose_name="user permissions",
-	)
+    is_staff = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)
+    is_salesperson = models.BooleanField(default=False)
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_set",
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_set",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
