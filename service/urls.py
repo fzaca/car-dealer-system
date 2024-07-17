3d09710a7a5d58458cb854837ee13460d62f8bc3
Zacarias <zaca03zaca@gmail.com>
2024-07-16 21:59:31 -0300

    Add core app for home view

--- service/urls.py
+++ service/urls.py
@@ -3,6 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
 	path("admin/", admin.site.urls),
+	# Core
+    path('', include('resources.core.pages.urls')),
 	# Users
 	path("api/users/", include("resources.users.api.urls")),
 	path("users/", include("resources.users.pages.urls")),
