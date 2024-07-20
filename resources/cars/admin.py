c71186071ed3dd8b5a05a1a8972ca055e125f0fd
Zacarias <zaca03zaca@gmail.com>
2024-07-20 01:16:19 -0300

    Add Car model

--- resources/cars/admin.py
+++ resources/cars/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from resources.cars.models import Brand, CarModel, Trim
+from resources.cars.models import Brand, Car, CarModel, Trim
 
 
 class BrandAdmin(admin.ModelAdmin):
@@ -16,11 +16,18 @@ class CarModelAdmin(admin.ModelAdmin):
 
 
 class TrimAdmin(admin.ModelAdmin):
-    list_display = ('name', 'car_model', 'year', 'price', 'fuel_type', 'engine_size')
+    list_display = ('name', 'car_model', 'year', 'potential_price', 'fuel_type', 'engine_size')
     search_fields = ('name', 'car_model__name', 'car_model__brand__name')
     ordering = ('car_model__brand__name', 'car_model__name', 'year')
+
+
+class CarAdmin(admin.ModelAdmin):  # FIXME: Que muestre solo las versiones del car model seleccionado
+    list_display = ('car_model', 'trim', 'year', 'price', 'color', 'registration_year', 'mileage')
+    search_fields = ('car_model__name', 'trim__name', 'color')
+    ordering = ('car_model__brand__name', 'car_model__name', 'trim__name', 'year')
 
 
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(CarModel, CarModelAdmin)
 admin.site.register(Trim, TrimAdmin)
+admin.site.register(Car, CarAdmin)
--- resources/cars/tests/test_models.py
+++ resources/cars/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from resources.cars.models import Brand, CarModel, Trim
+from resources.cars.models import Brand, Car, CarModel, Trim
 
 
 @pytest.mark.django_db  # noqa: PT023
@@ -24,8 +24,33 @@ def test_trim_str():
         car_model=car_model,
         name="230i",
         year=2022,
-        price=35000.00,
+        potential_price=None,
         fuel_type="Gasoline",
         engine_size=2.00
     )
     assert str(trim) == "230i (2022)"
+
+
+@pytest.mark.django_db  # noqa: PT023
+def test_car_str():
+    brand = Brand.objects.create(name="BMW")
+    car_model = CarModel.objects.create(name="Serie 2 Coupe", brand=brand)
+    trim = Trim.objects.create(
+        car_model=car_model,
+        name="230i",
+        year=2022,
+        potential_price=None,
+        fuel_type="Gasolina",
+        engine_size=2.00
+    )
+    car = Car.objects.create(
+        car_model=car_model,
+        trim=trim,
+        year=2022,
+        price=35000.00,
+        image_url="https://example.com/path/to/image.jpg",  # FIXME: Bucket url
+        color="Black",
+        registration_year=2022,
+        mileage=0
+    )
+    assert str(car) == "Serie 2 Coupe 230i (2022)"
