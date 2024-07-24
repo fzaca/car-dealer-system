a6edf67882da32de205124ac126a046eccddc9e8
Zacarias <zaca03zaca@gmail.com>
2024-07-23 23:55:35 -0300

    Fix statif file settings

--- service/settings/base.py
+++ service/settings/base.py
@@ -87,8 +87,8 @@ TIME_ZONE = "America/Argentina/Buenos_Aires"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
