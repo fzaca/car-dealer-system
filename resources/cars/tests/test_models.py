4bd69fcce96907a73f50d649512cc4156f717716
Zacarias <zaca03zaca@gmail.com>
2024-07-27 17:14:47 -0300

    Fix car tests

--- resources/cars/tests/test_models.py
+++ resources/cars/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from resources.cars.models import Brand, Car, CarModel
+from resources.cars.models import BodyType, Brand, Car, CarModel
 
 
 @pytest.mark.django_db  # noqa: PT023
@@ -17,11 +17,19 @@ def test_car_model_str():
 
 
 @pytest.mark.django_db  # noqa: PT023
+def test_bodytype_str():
+    body_type = BodyType(name="Coupe")
+    assert str(body_type) == "Coupe"
+
+
+@pytest.mark.django_db  # noqa: PT023
 def test_car_str():
     brand = Brand.objects.create(name="BMW")
     car_model = CarModel.objects.create(name="Serie 2 Coupe", brand=brand)
+    body_type = BodyType.objects.create(name="Coupe")
     car = Car.objects.create(
         car_model=car_model,
+        body_type=body_type,
         price=35000.00,
         engine_size=2.00,
         image_url="https://example.com/path/to/image.jpg",
@@ -31,7 +39,6 @@ def test_car_str():
         year=2022,
         mileage=0,
         seats=4,
-        doors=2,
-        body_type="Coupe"
+        doors=2
     )
     assert str(car) == "Serie 2 Coupe (2022)"
