8c1026db5d803b5bd2ce632f33080277c8c13265
Zacarias <zaca03zaca@gmail.com>
2024-07-27 13:33:29 -0300

    Add image field to cars

--- resources/cars/admin.py
+++ resources/cars/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from unfold.admin import ModelAdmin
 
 from resources.cars.forms import CarForm, CarModelForm
@@ -26,3 +27,18 @@ class CarAdmin(ModelAdmin):
     list_display = ('car_model', 'year', 'price', 'color', 'mileage', 'engine_size', 'gearbox', 'fuel_type', 'seats', 'doors', 'body_type')
     search_fields = ('car_model__name', 'car_model__brand__name', 'color', 'gearbox', 'fuel_type', 'body_type')
     ordering = ('car_model__brand__name', 'car_model__name', 'year')
+    readonly_fields = ('image_tag',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('image_tag',)
+        }),
+        ('Car Details', {
+            'fields': ('car_model', 'price', 'engine_size', 'image_url', 'gearbox', 'fuel_type', 'color', 'year', 'mileage', 'seats', 'doors', 'body_type')
+        }),
+    )
+
+    def image_tag(self, obj):  # noqa: PLR6301
+        return format_html('<img src="{}" width="300" height="200" />'.format(obj.image_url))
+
+    image_tag.short_description = 'Car Image'
