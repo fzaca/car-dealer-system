2c3feb6e36820ebbca5edbdfe749f7b697f97b10
Zacarias <zaca03zaca@gmail.com>
2024-07-27 19:18:53 -0300

    Add PaymentMethod, Payment, and Invoice models

--- resources/sales/models.py
+++ resources/sales/models.py
@@ -13,3 +13,20 @@ class Sale(models.Model):
         super().save(*args, **kwargs)
         self.car.is_available = False
         self.car.save()
+
+
+class PaymentMethod(models.Model):  # FIXME: Add commands for load data
+    name = models.CharField(max_length=50)
+
+
+class Payment(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    method = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Invoice(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    pdf = models.URLField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
