da208f1197c7cfe88de2bf810081282832e1d1ea
Zacarias <zaca03zaca@gmail.com>
2024-07-28 14:59:25 -0300

    Refactor image upload

--- resources/cars/admin.py
+++ resources/cars/admin.py
@@ -77,32 +77,38 @@ class CarAdmin(ModelAdmin):
     def save_model(self, request, obj, form, change):
         image_file = form.cleaned_data.get('image_file')
         if image_file:
-            file_ext = image_file.content_type.split("/")[-1]
-            file_name = f"cars/{uuid.uuid4()}.{file_ext}"
-
-            if obj.pk and obj.image_url:
-                old_file_name = obj.image_url.split('/')[-1]
-                try:
-                    minio_client.remove_object(MINIO_BUCKET, old_file_name)
-                except S3Error as e:
-                    self.message_user(request, f"Error deleting old image: {e}", level='error')
-
-            try:
-                minio_client.put_object(
-                    MINIO_BUCKET,
-                    file_name,
-                    image_file.file,
-                    length=image_file.size,
-                    content_type=image_file.content_type,
-                )
-                if settings.ENV == "local":
-                    public_url = f"http://{MINIO_PUBLIC_URL}/{MINIO_BUCKET}/{file_name}"
-                else:
-                    public_url = f"{MINIO_PUBLIC_HOST}/{MINIO_BUCKET}/{file_name}"
-                obj.image_url = public_url
-            except S3Error as e:
-                self.message_user(request, f"Error uploading image: {e}", level='error')
+            self._handle_image_upload(request, obj, image_file)
         super().save_model(request, obj, form, change)
+
+    def _handle_image_upload(self, request, obj, image_file):
+        file_ext = image_file.content_type.split("/")[-1]
+        file_name = f"cars/{uuid.uuid4()}.{file_ext}"
+
+        if obj.pk and obj.image_url:
+            old_file_name = obj.image_url.split('/')[-1]
+            try:
+                minio_client.remove_object(MINIO_BUCKET, old_file_name)
+            except S3Error as e:
+                self.message_user(request, f"Error deleting old image: {e}", level='error')
+
+        try:
+            minio_client.put_object(
+                MINIO_BUCKET,
+                file_name,
+                image_file.file,
+                length=image_file.size,
+                content_type=image_file.content_type,
+            )
+            public_url = self._generate_public_url(file_name)
+            obj.image_url = public_url
+        except S3Error as e:
+            self.message_user(request, f"Error uploading image: {e}", level='error')
+
+    def _generate_public_url(self, file_name):  # noqa: PLR6301
+        if settings.ENV == "local":
+            return f"http://{MINIO_PUBLIC_URL}/{MINIO_BUCKET}/{file_name}"
+        else:
+            return f"{MINIO_PUBLIC_HOST}/{MINIO_BUCKET}/{file_name}"
 
     # Unfold
     compressed_fields = True
