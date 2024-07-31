e915770bb10ad00601b33e91191bde9aa8e0ede8
Zacarias <zaca03zaca@gmail.com>
2024-07-31 19:29:23 -0300

    Optimize comments and update filters

--- resources/reviews/admin.py
+++ resources/reviews/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.core.cache import cache
 from unfold.admin import ModelAdmin
+from unfold.contrib.filters.admin import RangeDateTimeFilter
 
 from resources.reviews.models import Comment, Review
 
@@ -7,9 +9,27 @@ from resources.reviews.models import Comment, Review
 @admin.register(Comment)
 class CommentAdmin(ModelAdmin):
     list_display = ('hash', 'customer', 'car', 'created_at', 'updated_at')
-    list_filter = ('car', 'created_at', 'updated_at')
-    search_fields = ('customer__user', 'car__car_model__name', 'content', 'car')
+    search_fields = ('customer__user__username', 'car__model')
     readonly_fields = ('created_at', 'updated_at', 'hash')
+    list_filter = (
+        ('created_at', RangeDateTimeFilter),
+        ('updated_at', RangeDateTimeFilter),
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('customer', 'car', 'content', 'hash')
+        }),
+        ('Dates', {
+            'fields': ('created_at', 'updated_at'),
+        }),
+    )
+
+    def get_queryset(self, request):
+        queryset = cache.get('comment_queryset')
+        if not queryset:
+            queryset = super().get_queryset(request).select_related('customer', 'car')
+            cache.set('comment_queryset', queryset, timeout=60 * 15)  # Cache por 15 minutos
+        return queryset
 
 
 @admin.register(Review)
