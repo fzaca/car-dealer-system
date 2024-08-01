93fea1060715b1e2d8ec3d0342253c022985a26b
Zacarias <zaca03zaca@gmail.com>
2024-08-01 19:41:08 -0300

    Add table for sales and featured cars in dashboard

--- resources/core/pages/views.py
+++ resources/core/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from resources.cars.models import Car
+from resources.cars.models import Car, FeaturedCar
 from resources.sales.models import Sale
 
 
@@ -12,18 +12,27 @@ def dashboard_callback(request, context):
     cars_count = Car.objects.count()
     sales_count = Sale.objects.count()
 
-    available_cars = Car.objects.filter(is_available=True)[:10]
+    featured_cars = FeaturedCar.objects.select_related('car').all()[:10]
+
+    recent_sales = Sale.objects.order_by('-date')[:10]
 
     context.update({
         "cards": [
             {"title": "Total Cars", "metric": cars_count},
             {"title": "Total Sales", "metric": sales_count},
         ],
-        "table_data": {
+        "cars_table_data": {
             "headers": ["Car Model", "Price", "Availability"],
             "rows": [
-                [car.car_model.name, car.price, "Available" if car.is_available else "Sold"]
-                for car in available_cars
+                [car.car.car_model.name, car.car.price, "Featured"]
+                for car in featured_cars
+            ]
+        },
+        "sales_table_data": {
+            "headers": ["Sale ID", "Car Model", "Customer", "Date"],
+            "rows": [
+                [sale.hash, sale.car.car_model.name, sale.customer.user.username, sale.date.strftime("%Y-%m-%d %H:%M")]
+                for sale in recent_sales
             ]
         }
     })
