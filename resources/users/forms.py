288390f9e0286c906e4a28213ca3d5bc235befd2
Zacarias <zaca03zaca@gmail.com>
2024-08-04 01:16:17 -0300

    Improve login page

--- resources/users/forms.py
+++ resources/users/forms.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 from .models import CustomUser
 
@@ -19,3 +21,20 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email", "password1", "password2")
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Username or Email',
+        widget=forms.TextInput(attrs={'autofocus': True, 'class': 'input input-bordered'})
+    )
+    password = forms.CharField(
+        label='Password',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'input input-bordered'}),
+    )
+
+    def clean(self):
+        # Custom clean method if additional validation is needed
+        cleaned_data = super().clean()
+        return cleaned_data
--- resources/users/pages/views.py
+++ resources/users/pages/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import redirect, render
-from resources.users.forms import UserRegisterForm
+from resources.users.forms import UserRegisterForm, CustomAuthenticationForm
 from resources.users.models import Customer, CustomUser  # noqa: F401
 
 
@@ -23,15 +23,29 @@ def register_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("home")
-        else:
-            messages.error(request, "Invalid username or password.")
-    return render(request, "login.html")
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username_or_email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username_or_email, password=password)
+
+            if user is None:
+                try:
+                    user_instance = CustomUser.objects.get(email=username_or_email)
+                    user = authenticate(request, username=user_instance.username, password=password)
+                except CustomUser.DoesNotExist:
+                    user = None
+
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username/email or password.")
+    else:
+        form = CustomAuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
 
 
 def logout_view(request):
